093b5ae290b1ab76d3323274f297a2c62d7e9f75
s179066 <s179066@SEA-55E1DA>
2017-08-14 12:46:01 -0700

    Add project files.

---
+++ 1-HelloAIE/main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "Hello AIE"<< std::endl;
+	//prints the text in the quotations and 
+	std::cout << "" << std::endl;
+
+	system("pause");
+	//pauses the program
+
+	std::cout << "" << std::endl;
+	std::cout << "The Suffering by Coheed and Cambria" << std::endl;
+	std::cout << "The Joker by the Steve Miller Band" << std::endl;
+	std::cout << "Santeria by Sublime" << std::endl;
+	std::cout << "The Fixer by Pearl Jam" << std::endl;
+	std::cout << "" << std::endl;
+
+	system("pause");
+
+	std::cout << "" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "" << std::endl;
+
+	system("pause");
+
+	std::cout << "" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "" << std::endl;
+
+	system("pause");
+
+	//Problem A
+	std::cout << "SyntacticalErrors-A)" << std::endl;
+	std::cout << "Howdy AIE" << std::endl;
+	std::cout << "It's a fine day today as any!" << std::endl;
+	std::cout << "I really wish I had some potatoes..." << std::endl;
+
+	//Problem B
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+
+	system("pause");
+
+
+
+
+}
